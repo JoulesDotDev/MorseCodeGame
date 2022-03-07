4b19f8ae6648bb9641eae70302bc945bd5315e78
JoulesZapata <jouleszapata@gmail.com>
2022-03-07 20:37:58 +0100

    builder

--- build.js
+++ build.js
@@ -4,7 +4,9 @@ const path = require('path')
 const filename = path.resolve('./dist/public/index.html')
 fs.readFile(filename, 'utf8', function (err, data) {
 	if (err) throw err
-	const newData = data.replace('client.js', 'client.js?v=' + Date.now())
+	const timeStamp = Date.now()
+	const newData = data.replace('client.js', 'client.js?v=' + timeStamp)
+	.replace('all.css', 'all.css?v=' + timeStamp)
 
 	fs.writeFile(filename, newData, 'utf8', function (err) {
 		if (err) console.log(err)
